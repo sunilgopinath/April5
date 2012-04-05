83909f34e39e22dd0be3a197602b1876c9f0bf0b
Sunil Gopinath <sxg282@gmail.com>
2012-04-05 08:28:55 -0400

    draw a path and fill it

--- View.m
+++ View.m
@@ -15,7 +15,7 @@
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
-        self.backgroundColor = [UIColor redColor];
+        self.backgroundColor = [UIColor whiteColor];
 
     }
     return self;
@@ -39,32 +39,27 @@
           self.bounds.size.width,
           self.bounds.size.height
           );
-	// Drawing code
-	//Fill the Red Cross.
+	//Fill a right triangle.
 	CGSize size = self.bounds.size;
 	CGFloat min = MIN(size.width, size.height);
-	CGFloat longSide = min * 15 / 16;
-	CGFloat shortSide = longSide / 3;
+	CGFloat length = min * 5 / 8;           //of side
     
 	CGContextRef c = UIGraphicsGetCurrentContext();
-	CGContextBeginPath(c);
     
-	CGContextTranslateCTM(c, size.width / 2, size.height / 2); //origin at center of view
-	CGContextScaleCTM(c, 1, -1);                               //make Y axis point up
-    
-	CGRect horizontal = CGRectMake(-longSide / 2, -shortSide / 2, longSide, shortSide);
-	CGContextAddRect(c, horizontal);
-    
-	CGContextSetRGBFillColor(c, 1.0, 1.0, 1.0, 1.0);
-	CGContextFillPath(c);
+	//origin at right angle
+	CGContextTranslateCTM(c,
+                          (size.width + length) / 2,
+                          (size.height + length) / 2
+                          );
+	CGContextScaleCTM(c, 1, -1);
     
 	CGContextBeginPath(c);
-	CGContextRotateCTM(c, M_PI / 2);	//90 degrees clockwise
-    //Rotation of 15°, negative for counterclockwise.
-	CGContextRotateCTM(c, -15.0 * M_PI / 180.0);
-	CGContextAddRect(c, horizontal);
+	CGContextMoveToPoint(c, 0, 0);          //lower right vertex (the right angle)
+	CGContextAddLineToPoint(c, 0, length);  //upper right vertex
+	CGContextAddLineToPoint(c, -length, 0); //lower left vertex
+	CGContextClosePath(c);                  //back to starting point
     
-	CGContextSetRGBFillColor(c, 1.0, 1.0, 1.0, 1.0);
+	CGContextSetRGBFillColor(c, 1.0, 0.0, 0.0, 1.0);
 	CGContextFillPath(c);
 }
 
